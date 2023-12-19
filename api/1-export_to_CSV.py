76b927da9611b5b5c304adf6b2fb969e855950f7
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-19 17:23:51 -0400

    Refactor API data export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Using employee ID returns information about his/her TODO list progress"""
+"""Gather data from an API"""
+
 import csv
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    response = requests.get(url)  # Get the response from the API
-    name = response.json().get("name")  # Get the name of the user
+    response = requests.get(url)
+    name = response.json().get("username")
     url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         argv[1])
-    # Get the response from the API for the user_id from the argv
     response = requests.get(url)
-    tasks = response.json()  # Get the tasks of the user
+    tasks = response.json()
     with open("{}.csv".format(argv[1]), "w") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in tasks:  # Write the tasks in the CSV file
+        for task in tasks:
             writer.writerow([argv[1], name, task.get("completed"),
-                            task.get("title")])  # Write the row
+                             task.get("title")])
