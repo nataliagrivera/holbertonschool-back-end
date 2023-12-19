e53bf5c6c9536b574d05de4ece1d85a3d4e5fe89
nataliagrivera <nataliagirvera62@gmail.com>
2023-12-19 15:48:10 -0400

    Refactor API data gathering code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,13 +7,11 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
     response = requests.get(url)  # Get the response from the API
     name = response.json().get("name")  # Get the name of the user
-
     url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         argv[1])
     # Get the response from the API for the user_id from the argv
     response = requests.get(url)
     tasks = response.json()  # Get the tasks of the user
-
     completed = [task for task in tasks if task.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(name, len(completed),
                                                           len(tasks)))
